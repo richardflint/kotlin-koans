218faa4e3d715b2e6a1f123702baf9c94cfd9f45
Richard Flint <richard@richardflint.co.uk>
2018-02-13 21:26:48 +0000

    answers for ii exercises

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -10,7 +10,4 @@ fun example8() {
     negative == listOf(-4, -11)
 }
 
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
-    // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
-}
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.partition { it.orders.count { !it.isDelivered } > it.orders.count { it.isDelivered } }.first.toSet()
